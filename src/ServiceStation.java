f7966ff6407d830173a5fb4921a06b0a3a7d894f
kirillstrizenko <krealstrzh@icloud.com>
2022-09-07 19:07:00 +0300

    Homework 2_3

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,28 +1,5 @@
-public class ServiceStation implements ServiceStationInterface {
+import com.sun.jdi.connect.Transport;
 
-    public void check (Bicycle bicycle) {
-        checkBasic(bicycle);
-    }
-    public void check (Car car) {
-        if (car != null) {
-            checkBasic(car);
-            car.checkEngine();
-        }
-    }
-    public void check (Truck truck) {
-        if (truck != null) {
-            checkBasic(truck);
-            truck.checkEngine();
-            truck.checkTrailer();
-        }
-    }
-
-    private void checkBasic(Bicycle bicycle) {
-        if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.getModelName());
-            for (int i = 0; i < bicycle.getWheelsCount(); i++) {
-                bicycle.updateTyre();
-            }
-        }
-    }
+public interface ServiceStation {
+    void check();
 }
